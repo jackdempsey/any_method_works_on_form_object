08b7fec1bb3fc00a1d61a849f22756c43d9fffca
Jack Dempsey <jack.dempsey@gmail.com>
2011-04-26 09:42:23 -0700

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Footest::Application.config.secret_token = 'dbc6d5ab2a48994805265b6ac6d670b8ee283ed879b31bc246a532f0c2c5c74d3d3fbc96fd1043a42278b956b0ddb547575faf383a232e795bd0e1de65778cd8'
